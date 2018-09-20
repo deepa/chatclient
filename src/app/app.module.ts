6fd4845ddca609fa288566747aa50811292c1fcb
Deepalakshmi Thiagarajan <deepa.name@gmail.com>
2018-09-19 19:58:26 -0700

    add websocket service, add imports and providers to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatList } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { WebSocketService } from './web-socket.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [WebSocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/web-socket.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+
+@Injectable()
+export class WebSocketService {
+
+    onMessage: Subject<any> = new Subject<any>();
+
+    websocket;
+
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        this.websocket = new WebSocket('ws://localhost:4200/chat');
+        this.websocket.onMessage = (message: MessageEvent) => {
+            this.onMessage.next(message);
+        };
+    }
+
+    send(message: string): void {
+        this.websocket.send(message);
+    }
+}
